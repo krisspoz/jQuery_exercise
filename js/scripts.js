53c8bacf247ff4a940f958f164911ce512318c33
Krzysztof Stankowski <Krzysztof Stankowski>
2017-02-08 13:48:54 +0100

    jQuery exercise

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+var span = $("span");
+
+span.each(function(index, element) {
+	if(index % 2 != 0) {
+		$(element).css('color', 'red');
+	};
+});
+
+// $("span:odd").css('color','red'); // krótszy zapis alternatywny
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+$(element).append(button) 
+
+});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
